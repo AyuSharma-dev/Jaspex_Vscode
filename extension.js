db971d4f0e51a55dff21607ce353773e49d4b61d
Ayush Sharma <sharma.ayush84444@gmail.com>
2020-03-08 21:59:55 +0530

    A working version.

---
+++ extension.js
@@ -0,0 +1,65 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "Jaspex" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.generateapex', function() {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        //const editor = vscode.window.activeTextEditor;;
+        //const text = editor.document.getText(editor.selection);
+        var unirest = require("unirest");
+        const clipboardy = require('clipboardy')
+
+        var req = unirest("GET", "https://jaspex.herokuapp.com/api/generateApexAPI")
+
+        req.headers({
+            "cache-control": "no-cache",
+            'Content-Type': 'application/json'
+        })
+        const editor = vscode.window.activeTextEditor;
+        const jsonBody = editor.document.getText(editor.selection);
+
+        req.send({ "Body": jsonBody })
+
+        req.end(function(res) {
+            if (res.error) throw new Error(res.error)
+            const msg = res.body;
+            if (msg == 'Error in Json decoding, please check.') {
+                vscode.window.showErrorMessage(msg);
+                //vscode.window.showInformationMessage(msg);
+            } else {
+                clipboardy.writeSync(msg);
+                vscode.window.showInformationMessage('Apex generator created successfully \n and copied to clip board.');
+            }
+            console.log(res.body)
+        })
+
+    });
+
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
